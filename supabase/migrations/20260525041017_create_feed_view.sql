af26506f34a1668edd658fee400d164bb81f532e
hecharli <charhern1676@gmail.com>
2026-05-27 13:38:36 -0400

    post fetcher in progress

--- supabase/migrations/20260525041017_create_feed_view.sql
+++ supabase/migrations/20260525041017_create_feed_view.sql
@@ -11,18 +11,18 @@ where (
 order by p.date desc;
 
 
-drop view if exists feed_posts;
+-- drop view if exists feed_posts;
 
-create view feed_posts as
-select p.*, f.following_id, u.name, u.username, u.pfp_path from posts as p
-join following as f on f.following_id = p.user_id
-join users as u on u.id = f.following_id
-where (
-    auth.uid() = f.follower_id
-    and f.approved = true
-    and p.user_id != auth.uid()
-)
-order by p.date desc;
+-- create view feed_posts as
+-- select p.*, f.following_id, u.name, u.username, u.pfp_path from posts as p
+-- join following as f on f.following_id = p.user_id
+-- join users as u on u.id = f.following_id
+-- where (
+--     auth.uid() = f.follower_id
+--     and f.approved = true
+--     and p.user_id != auth.uid()
+-- )
+-- order by p.date desc;
 
 -- create view feed_posts as
 -- select 
