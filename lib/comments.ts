b0538d910a8e9cfa195f3235d87543782bd78660
hecharli <charhern1676@gmail.com>
2026-05-31 12:54:55 -0400

    still need to add username to comments but not bad otherwise

--- lib/comments.ts
+++ lib/comments.ts
@@ -3,7 +3,7 @@ import { SupabaseClient } from "@supabase/supabase-js";
 export type NewComment = {
     post_id: string,
     user_id: string,
-    parent_comment_id?: string,
+    parent_comment_id?: string | null,
     comment: string
 };
 
