e9f703531433251f991b0633cc6e666caf79618b
Charlie-H7 <105665655+Charlie-H7@users.noreply.github.com>
2026-07-08 23:18:47 -0400

    feat: add initial post upload implementation (#2)
    - add post upload flow
    - add storage bucket RLS policies
    - add required database migrations
    
    Known issue:
    - post creation currently fails due to a recursive RLS policy

--- lib/posts.ts
+++ lib/posts.ts
@@ -12,6 +12,12 @@ export type PostRow = {
     date: string;
 }
 
+export type PostInsert = {
+    user_id: string;
+    caption?: string; // I don't think I need to make like a thing here for the caption as insta doesn't have a caption
+    image_path: string;
+}
+
 export type PostFetch = {
     id: string; // post id
     username: string;
@@ -24,18 +30,28 @@ export type PostFetch = {
 }
 
 // 1. Need to be able to submit posts to the db for others to see
+// Return the id od the post so I can route to it after submission
 // 2. Need to be able to fetch posts based on if you are following people or not
 
 // Policies only allow people to submit their own posts so no additional logic
-export async function uploadPost(supabase: SupabaseClient, {id, user_id, image_path}: PostRow) {
+export async function uploadPost(supabase: SupabaseClient, {user_id, image_path}: PostInsert) {
+    const { data: { user } } = await supabase.auth.getUser();
+    console.log("auth user id:", user?.id);
+    console.log("post user_id:", user_id);
     const { data, error } = await supabase.from("posts")
     .insert({
-        id,
         user_id,
         image_path,
     })
+    .select()
+    .single();
+
+    console.log("insert error:", error);
+    console.log("error details:", error?.details);
+    console.log("error hint:", error?.hint);
 
     if(error){ console.log(error.message); return; }
+    return data;
 }
 
 
